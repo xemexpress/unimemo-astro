b72fa06538cb9f1115b0fd683141f84822aa33aa
Jason Luo <22487340+xemexpress@users.noreply.github.com>
2024-07-26 00:11:04 +0800

    Updated ProductCard and took out the login route temporarily

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,34 @@
 // src/middleware.ts
 
 import { defineMiddleware } from "astro/middleware";
-import { projectAuth } from "./firebase/client";
-import { ROUTES, PUBLIC_ROUTES } from "./utils/routes";
+// import { projectAuth } from "./firebase/client";
+// import { ROUTES, PUBLIC_ROUTES } from "./utils/routes";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-    const isLoggedIn = projectAuth.currentUser;
-    const { pathname } = context.url;
+export const onRequest = defineMiddleware(async (_, next) => {
+    // export const onRequest = defineMiddleware(async (context, next) => {
+    // const isLoggedIn = projectAuth.currentUser;
+    // const { pathname } = context.url;
 
-    const isPublicRoute = PUBLIC_ROUTES.some((route) => {
-        if (route.includes(":slug")) {
-            const dynamicRoute = route.replace(":slug", ".*");
-            const regex = new RegExp(`^${dynamicRoute}$`);
-            return regex.test(pathname);
-        }
+    // const isPublicRoute = PUBLIC_ROUTES.some((route) => {
+    //     if (route.includes(":slug")) {
+    //         const dynamicRoute = route.replace(":slug", ".*");
+    //         const regex = new RegExp(`^${dynamicRoute}$`);
+    //         return regex.test(pathname);
+    //     }
 
-        return route === pathname;
-    });
+    //     return route === pathname;
+    // });
 
-    if (
-        isLoggedIn &&
-        (pathname === ROUTES.CREATE_ACCOUNT || pathname === ROUTES.LOGIN)
-    ) {
-        return context.redirect(ROUTES.HOME);
-    }
+    // if (
+    //     isLoggedIn &&
+    //     (pathname === ROUTES.CREATE_ACCOUNT || pathname === ROUTES.LOGIN)
+    // ) {
+    //     return context.redirect(ROUTES.HOME);
+    // }
 
-    if (!isLoggedIn && !isPublicRoute && context.request.method === "GET") {
-        return context.redirect(ROUTES.LOGIN);
-    }
+    // if (!isLoggedIn && !isPublicRoute && context.request.method === "GET") {
+    //     return context.redirect(ROUTES.LOGIN);
+    // }
 
     return next();
 });
